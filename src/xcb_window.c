6b267dc3fd1bca529143f387fa0d9612e78bd5c7
Ellen Marie Dash <me@duckie.co>
2018-05-20 21:02:37 -0400

    Initialize array properly.

--- src/xcb_window.c
+++ src/xcb_window.c
@@ -112,7 +112,7 @@ bool cadel_xcb_reparent_windows(xcb_connection_t *connection,
 {
     xcb_window_t child;
 
-    cadel_xcb_window_list_t children = {{0,},};
+    cadel_xcb_window_list_t children = {{0,}, 0};
     if (!cadel_xcb_query_tree(&children, connection, root)) {
         return false;
     }
